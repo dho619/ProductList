69d791d8eb49aafd8db889b38dd6f94c50305306
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2019-10-04 10:19:28 -0300

    Api CRUD em node, criada

---
+++ node-api/server.js
@@ -0,0 +1,22 @@
+const express = require('express'); //para fazer rotas
+const cors = require('cors');
+const mongoose = require('mongoose'); // para conexao com o mongodb
+const requireDir = require('require-dir'); // faz a importacoes de models automaticamente
+
+const app = express();//Iniciando o APP
+app.use(express.json());
+app.use(cors());
+
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    {useNewUrlParser: true}
+);
+requireDir('./src/models'); //mostrando o diretorio dos models
+
+
+
+//Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
