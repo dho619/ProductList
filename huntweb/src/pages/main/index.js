2ba855bcbf94083c160c3e16dff92744033bf5b6
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2019-10-07 16:40:20 -0300

    Adicionando ReactJS

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import api from '../../services/api'; //api criada com node
 
 export default class Main extends Component {
-//componentDidMount executa assim que e criado a pagina
-    componentDidMount() {
-        this.loadProducts();
+    // usa-se o state para poder acessar essas variaveis externamente
+    state = { //state e um objeto
+        products: [],
     }
 
+//componentDidMount executa assim que e criado a pagina
+    componentDidMount() { //nao clk o "() => ", pq e uma funcao da proprio js
+        this.loadProducts();
+    }
+ 
     loadProducts = async () => {
         const response = await api.get('/products');
-
-        console.log(response.data.docs);
+        //setState e para alterar o objeto state
+        this.setState({ products: response.data.docs }); //response.data.docs, ta pegando os produtos na api
     };
 
+    //render sempre executa novamente, se alguma variavel do state for alterada
     render() {
-        return  <h1>Hello Word!</h1>
+        //a key no h2 e passada, pq o react pede que tenha uma key unica pra cada item da iteracao
+        return  (
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        )
     }
 }
